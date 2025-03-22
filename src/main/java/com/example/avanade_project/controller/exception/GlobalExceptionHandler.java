15162e2b4d9eef67f6e1a6d927ed3d5dd29bc88c
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-22 10:33:44 -0300

    fix : handles ConstraintViolationException

--- src/main/java/com/example/avanade_project/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/avanade_project/controller/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.avanade_project.controller.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -24,10 +25,18 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>("Resource ID not found.", HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+        public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException constraintException) {
+            return new ResponseEntity<>(constraintException.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
         var message = "Unexpected server error, see the logs.";
         logger.error(message, unexpectedException);
         return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+
 }
