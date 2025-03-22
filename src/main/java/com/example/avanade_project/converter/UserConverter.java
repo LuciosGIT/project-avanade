faebab8d29a588137b2244f92ffde839d141227a
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-22 13:28:03 -0300

    feat : add UserConverter class to DTO conversion

--- src/main/java/com/example/avanade_project/converter/UserConverter.java
+++ src/main/java/com/example/avanade_project/converter/UserConverter.java
@@ -1,2 +1,20 @@
-package com.example.avanade_project.converter;public class UserConverter {
+package com.example.avanade_project.converter;
+
+import com.example.avanade_project.domain.model.User;
+import com.example.avanade_project.dtos.UserDTO;
+
+public class UserConverter {
+
+    public static User ConvertUserDTOtoUser(UserDTO userDto) {
+        User user = new User();
+
+        user.setAccount(userDto.account());
+        user.setNews(userDto.news());
+        user.setName(userDto.name());
+        user.setCard(userDto.card());
+        user.setFeatures(userDto.features());
+
+        return user;
+    }
+
 }
