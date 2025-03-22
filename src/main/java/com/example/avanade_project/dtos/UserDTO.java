dc2173089938273c9d1e0655531c0e8515b59491
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-22 13:25:55 -0300

    feat : creates User DTO to data transfer

--- src/main/java/com/example/avanade_project/dtos/UserDTO.java
+++ src/main/java/com/example/avanade_project/dtos/UserDTO.java
@@ -1,2 +1,11 @@
-package com.example.avanade_project.dtos;public record UserDTO() {
+package com.example.avanade_project.dtos;
+
+import com.example.avanade_project.domain.model.Account;
+import com.example.avanade_project.domain.model.Card;
+import com.example.avanade_project.domain.model.Feature;
+import com.example.avanade_project.domain.model.News;
+
+import java.util.List;
+
+public record UserDTO(String name, Account account, Card card, List<Feature> features, List<News> news) {
 }
