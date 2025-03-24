8321bb573ab3d3daf45229a1adc3af1978ac852e
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-24 19:57:12 -0300

    feat : add swagger documentation on API

--- src/main/java/com/example/avanade_project/controller/UserController.java
+++ src/main/java/com/example/avanade_project/controller/UserController.java
@@ -5,6 +5,10 @@ import com.example.avanade_project.domain.model.User;
 import com.example.avanade_project.dtos.UserDTO;
 import com.example.avanade_project.dtos.UserPageDTO;
 import com.example.avanade_project.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -24,6 +28,7 @@ import java.util.stream.Collectors;
 @Validated
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users Controller", description = "RESTful API for managing users.")
 public class UserController {
 
     private final UserService userService;
@@ -33,6 +38,10 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all users", description = "Retrieve a list of all registered users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation successful")
+    })
     public ResponseEntity<UserPageDTO> findAll(@RequestParam("page") @PositiveOrZero int page,
                                               @RequestParam("pageSize") @PositiveOrZero @Max(100) int pageSize) {
 
@@ -42,12 +51,22 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a user by ID", description = "Retrieve a specific user based on its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation successful"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<UserDTO> findById(@PathVariable @Positive @NotNull Long id) {
         var user = userService.findById(id);
         return ResponseEntity.ok(user);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new user", description = "Create a new user and return the created user's data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "422", description = "Invalid user data provided")
+    })
     public ResponseEntity<UserDTO> create(@RequestBody @NotNull UserDTO userToCreate) {
 
         var userCreated = userService.create(userToCreate);
@@ -62,6 +81,11 @@ public class UserController {
     }
 
     @PutMapping("/update/{id}/{limit}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Card limit updated successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "422", description = "Invalid data provided")
+    })
     public ResponseEntity<UserDTO> updateCardLimit(@PathVariable @Positive @NotNull Long id , @PathVariable @Positive @NotNull BigDecimal limit) {
 
         User updatedUser = userService.updateCardLimit(id, limit);
@@ -70,6 +94,11 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a user", description = "Delete an existing user based on its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     @ResponseStatus(HttpStatus.OK)
     public void deleteUserById(@PathVariable @Positive @NotNull Long id) {
 
